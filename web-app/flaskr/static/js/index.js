dbe11352c72aedaaba9d1420af6f26c377ae477d
marait123 <max.ibrahim43@hotmail.com>
2022-04-19 23:55:16 +0200

    added the space button and reorganized the index.js and enabled navigation based on url hash change

--- web-app/flaskr/static/js/index.js
+++ web-app/flaskr/static/js/index.js
@@ -12,3 +12,20 @@ function send_command(endpoint) {
     },
   });
 }
+let currentTab = ".manualTab";
+function TabChanged(e = null) {
+  // Your Code goes here
+  let tabClassName = "." + window.location.hash.substring(1) + "Tab";
+  $(currentTab).hide();
+  currentTab = tabClassName;
+  $(currentTab).show();
+}
+$(function () {
+  TabChanged();
+  setInterval(function () {
+    // alert("hi");
+    $(".timeNow").text(new Date().toLocaleTimeString());
+  }, 10 / 00);
+
+  $(window).on("hashchange", TabChanged);
+});
