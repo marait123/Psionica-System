35446a0c03445f74b542124df7b54b3e2a276327
marait123 <max.ibrahim43@hotmail.com>
2022-07-12 20:33:37 +0200

    predicted class now working and added place holder for prediceted action

--- web-app/flaskr/static/js/simulation.js
+++ web-app/flaskr/static/js/simulation.js
@@ -30,6 +30,17 @@ let action_uiclass_map = {
   B:"#leg-imagine",
 }
 
+let pred_action_uiclass_map = {
+  L:"#pred-left-hand-imagine",
+  R:"#pred-right-hand-imagine",
+  F:"#pred-tongue-imagine",
+  B:"#pred-leg-imagine",
+}
+
+
+// animations area
+let timeout_id = null;
+
 function hide_animations(){
   for (const key in action_uiclass_map) {
     if (Object.hasOwnProperty.call(action_uiclass_map, key)) {
@@ -38,17 +49,32 @@ function hide_animations(){
       
     }
   }
+  
+  for (const key in pred_action_uiclass_map) {
+    if (Object.hasOwnProperty.call(pred_action_uiclass_map, key)) {
+      const element = pred_action_uiclass_map[key];
+        $(element).hide()
+      
+    }
+  }
+
+  timeout_id = null;
 }
 /*
 calling update ui with no paramters hides everything
 */
+
 function update_ui(true_action){
+  if(timeout_id ){
+    clearTimeout(timeout_id)
+    timeout_id = null;
+  }
 try {  
 
   if(true_action){
     hide_animations();
     $(action_uiclass_map[true_action]).show()
-    setTimeout(hide_animations, 2000);
+    timeout_id = setTimeout(hide_animations, 2000);
   }
 
 
@@ -68,6 +94,7 @@ function send_command( action ) {
       predicted_action = result.prediction;
       let endpoint= `${PHONE_IP}/${predicted_action}`
       console.log("we are sending to ", endpoint, predicted_action)
+      $(pred_action_uiclass_map[predicted_action]).show();
       $.ajax({
         type: "GET",
         url: endpoint, //TODO: update request URL
