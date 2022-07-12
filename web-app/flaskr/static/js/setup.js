bc0ed50c3ce88e9f4dc0731546df7dcb0dc86f0e
marait123 <max.ibrahim43@hotmail.com>
2022-07-12 17:22:53 +0200

    added the control and new animations and are working correctly with 2 sec

--- web-app/flaskr/static/js/setup.js
+++ web-app/flaskr/static/js/setup.js
@@ -1,25 +1,56 @@
-
-
-PHONE_IP = "http://192.168.1.160:12345"
-CAR_IP = "http://192.168.1.159:12345"
+// this is the file where the setup of everything happens
+// and the initial configurations are made
+PHONE_IP = "http://192.168.1.160:12345";
+CAR_IP = "http://192.168.1.159:12345";
 
 // PHONE_IP = "http://192.168.4.2:12345"
 // CAR_IP = "http://192.168.4.1:12345"
-PHONE_IP = CAR_IP
-console.log(PHONE_IP)
+PHONE_IP = CAR_IP;
+console.log(PHONE_IP);
+$(function () {
+  console.log("hello we are comming soon");
+  // setInterval(function () {
+  //   // alert("hi");
+  //   $(".timeNow").text(new Date().toLocaleTimeString());
+  // }, 10 / 00);
 
-function discoverip(){
-    for(let i = 1; i < 254;i++){
-        $.ajax({
-            type: "GET",
-            url: ``, //TODO: update request URL
-            success: (result) => {
-              console.log("result is ", result);
-            },
-            error: (error) => {
-              console.error(error);
-            },
-          });   
-    }
-    
-}
+  // $(window).on("hashchange", TabChanged);
+  // $(".control").on("keydown", function (e) {
+  //   console.log(e);
+  //   console.log("you pressed", e.key);
+  // });
+
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/simulation", //TODO: update request URL
+  //   success: (result) => {
+  //     // simulation_status = result.simulation_status;
+  //     const { simulation_status, last_action } = result;
+  //     console.log("get_simulation_status result is ", {
+  //       simulation_status,
+  //       last_action,
+  //     });
+
+  //   },
+  //   error: (error) => {
+  //     console.error(error);
+  //     alert(error);
+  //   },
+  // });
+});
+
+// function discoverip(){
+//     for(let i = 1; i < 254;i++){
+//         $.ajax({
+//             type: "GET",
+//             url: ``, //TODO: update request URL
+//             success: (result) => {
+//               console.log("result is ", result);
+//             },
+//             error: (error) => {
+//               console.error(error);
+//             },
+//           });
+//     }
+
+// }
